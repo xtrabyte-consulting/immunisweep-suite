5e5661c9a386eb6e11cac8cd7538f6f2e0997602
Bryce Kowalczyk <bkowalczyk@xtrabyteconsulting.com>
2024-10-01 22:29:15 -0400

    Get live plot working

--- LivePlot.py
+++ LivePlot.py
@@ -9,6 +9,7 @@ from matplotlib.animation import FuncAnimation
 class FrequencyPlot(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         # Create a Figure and Canvas
+        plt.ion()
         self.fig, self.ax = plt.subplots()
         super().__init__(self.fig)
         self.setParent(parent)
@@ -17,29 +18,29 @@ class FrequencyPlot(FigureCanvas):
         self.x_data = []
         self.y_data = []
         
-        # Initialize an empty line on the plot
-        self.line, = self.ax.plot(self.x_data, self.y_data, 'r-')
+        self.line, = self.ax.plot(self.x_data, self.y_data, '-g')
         
-        # Animation function
-        #self.anim = FuncAnimation(self.fig, self.update_plot, init_func=self.init_plot, interval=1000)
-        
-    def init_plot(self, x_min: float = 0.0, x_max: float = 100.0, y_min: float = 0.0, y_max: float = 6000.0):
+
+    def init_plot(self, x_min: float = 0.0, x_max: float = 100.0, y_min: float = 0.0, y_max: float = 500.0):
+        print("Initializing plot: x_min = {}, x_max = {}, y_min = {}, y_max = {}".format(x_min, x_max, y_min, y_max))
         self.ax.set_xlim(x_min, x_max)
         self.ax.set_ylim(y_min, y_max)
-        self.line.set_data([], [])
+        self.line.set_data(self.x_data, self.y_data)
+        self.ax.relim()
+        self.ax.autoscale_view()
+        self.ax.set_xlabel('Time (s)')
+        self.ax.set_ylabel('Frequency (Hz)')
+        self.ax.set_title('Frequency vs Time')
+        self.draw_idle()
         return self.line,
-    
-    def set_start_frequency(self, start_frequency: float):
-        self.ax.set_xlim(0.0, None)
-        self.ax.set_ylim(start_frequency, None)
-        
-    def set_stop_frequency(self, stop_frequency: float, steps: int, dwell_time: float):
-        self.ax.set_xlim(0.0, (steps * dwell_time))
-        self.ax.set_ylim(None, stop_frequency)
 
     def update_plot(self, x: float, y: float):
+        print("Updating plot: x = {}, y = {}".format(x, y))
         self.x_data.append(x)
         self.y_data.append(y)
         self.line.set_data(self.x_data, self.y_data)
-        #self.ax.relim()
+        self.ax.relim()
+        self.ax.autoscale_view()
+
+        self.draw_idle()
         return self.line,
