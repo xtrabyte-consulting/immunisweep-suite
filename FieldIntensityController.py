511c52178fa91158ebfb0ac743d9e3d895c4f98a
Bryce Kowalczyk <bkowalczyk@xtrabyteconsulting.com>
2024-10-03 20:21:56 -0400

    Move PIDController to its own module

--- FieldIntensityController.py
+++ FieldIntensityController.py
@@ -9,6 +9,7 @@ from MainWindow import Ui_MainWindow
 from SignalGenerator import AgilentN5181A, Time, Modulation, Frequency, SignalGenerator
 from FieldProbe import ETSLindgrenHI6006, FieldProbe
 from LivePlot import FrequencyPlot, PowerPlot
+from PID import PIDController
 
 import os
 import sys
@@ -27,43 +28,6 @@ try:
     QtWin.setCurrentProcessExplicitAppUserModelID(myappid)    
 except ImportError:
     pass
-
-class PIDController():
-    
-    def __init__(self, Kp, Ki, Kd):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
-        self.prev_error = 0.0
-        self.integral = 0.0
-        self.desired_field = 1.0
-        self.current_field = 0.0
-    
-    def setGains(self, Kp: float, Ki: float, Kd: float):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
-    
-    def setTargetValue(self, setpoint: float):
-        print(f"Desired field set to: {setpoint}")
-        self.desired_field = setpoint
-        
-    def getTargetValue(self) -> float:
-        return self.desired_field
-    
-    def calculate(self, current_field: float) -> float:
-        error = self.desired_field - current_field
-        self.integral += error
-        derivative = error - self.prev_error
-        output = (self.Kp * error) + (self.Ki * self.integral) + (self.Kd * derivative)
-        self.prev_error = error
-        print(f"Current Field: {current_field}, Error: {error}, Integral: {self.integral}, Derivative: {derivative}, Output: {output}")
-        return output
-    
-    def clear(self):
-        self.measured_value = 0.0
-        self.prev_error = 0.0
-        self.integral = 0.0
 
 class EquipmentLimits():
     
