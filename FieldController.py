cc7bef52ef629cbbdee0c9e4f58c7a9661d21648
Bryce Kowalczyk <bkowalczyk@xtrabyteconsulting.com>
2025-01-09 18:19:14 -0500

    Delete dead code

--- FieldController.py
+++ FieldController.py
@@ -27,9 +27,6 @@ class FieldController(QObject):
         if pid_controller is None:
             self.pid_controller = PID(0.1, 0.01, 0.01)
             self.use_stepper = True
-        
-        #self.adjust_timer = QTimer()
-        #self.adjust_timer.timeout.connect(self.adjust_power_step)
             
         # Initialize the field controller parameters
         self.is_sweeping = False
@@ -64,24 +61,12 @@ class FieldController(QObject):
         # Create a log file path with the current date and time
         log_file = os.path.join(log_dir, f"log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt")
         
-        '''
-        log_dir = os.path.join(os.getcwd(), "MyApp")
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-        log_file = os.path.join(log_dir, "FieldController.log")
-        '''
         return log_file
     
     def log_warning(self, warning: str):
         '''Log data to a file.'''
         with open(self.log_file_path, "a") as log_file:
             log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - High Field Detected Warning: {warning}\n")
-    
-    '''
-    def move_timers_to_thread(self, thread: QThread):
-        #Move the timers to this controller thread.
-        self.adjust_timer.moveToThread(thread)
-    '''    
             
     def start_sweep(self):
         """Start the frequency sweep with the specified range, dwell and step term."""
@@ -149,60 +134,6 @@ class FieldController(QObject):
             self.frequencyUpdated.emit(self.current_freq)
             self.sweepStatus.emit(100.0)
             
-    #def start_adjustment(self):
-    #    """Start the power adjustment process."""
-    #    self.adjust_timer.start(10) # Update every 10 ms to align with probe readings
-    '''   
-    def adjust_power_step(self):
-        """Adjust the power level based on probe readings to return to target field level."""
-        # Get the current field level from the field probe
-        current_field_level, x, y, z = self.field_probe.readCurrentField()
-        
-        print(f"Current Field Level: {current_field_level} V/m")
-        # Emit signal to update the UI with the field level
-        #self.fieldUpdated.emit(current_field_level, x, y, z)
-        
-        # Check if the field level is too high. If so, stop the sweep
-        # shut off the RF output and emit a signal to notify the user
-        if current_field_level > (self.target_field * 2.0):
-            self.highFieldDetected.emit()
-            self.signal_generator.setPower(self.base_power)
-            self.signal_generator.setRFOut(False)
-            self.is_sweeping = False
-            self.adjust_timer.stop()
-            return
-        
-        if (current_field_level > self.target_field) and (current_field_level < (self.target_field * self.threshold)):
-            print(f"Field level within threshold: {current_field_level}")
-            #current_field_level, x, y, z = self.field_probe.readCurrentField()
-            
-            self.fieldUpdated.emit(current_field_level, x, y, z)
-            self.adjust_timer.stop()
-            self.startDwell.emit(self.dwell_time_ms)
-            print("Power adjusted. Moving to next frequency step.")
-            self.current_freq = self.current_freq + (self.current_freq * self.sweep_term)
-            return
-        
-        self.current_power = self.signal_generator.getPower()
-        
-        if self.use_stepper:
-            print("Using stepper control")
-            # Adjust the power level based on the current field level
-            if current_field_level < self.target_field:
-                self.current_power += 0.1
-            elif current_field_level > (self.target_field * self.threshold):
-                self.current_power -= 1
-            #print(f"Setting power to: {current_power}")
-            # Move to the next frequency step
-            self.current_power = self.signal_generator.setPower(self.current_power)
-            print(f"Power set to: {self.current_power}")
-        else:
-            print("Using PID control")
-            pid_output = self.pid_controller.calculate(current_field_level)
-            self.signal_generator.setPower(pid_output + self.current_power)
-        self.powerUpdated.emit(self.current_power)
-    '''
-            
     def stop_sweep(self):
         """Stop the frequency sweep and all timers."""
         print("Stopping sweep")
@@ -261,34 +192,6 @@ class FieldController(QObject):
     def getSweepTime(self) -> float:
         return (self.dwell_time_ms / 1000) * self.getStepCount() * 1.1 # Assume 10% overhead for settling time
     
-    
-    '''
-            while current_freq <= self.stop_freq and self.is_sweeping:
-            print(f"Current Frequency: {current_freq}")
-            # Set the signal generator frequency
-            self.signal_generator.setFrequency(current_freq, Frequency.MHz.value)
-            self.sweepStatus.emit(self.log_percentage(current_freq, self.start_freq, self.stop_freq))
-
-            # Emit the signal to update the UI with the new frequency
-            self.frequencyUpdated.emit(current_freq)
-
-            print(f"Adjusting power to target level at {current_freq} MHz")
-            # Perform closed-loop control to adjust the power
-            self.adjust_power_to_target_level()
-            
-            print(f"Sleeping for {self.dwell_time} seconds")
-            sleep(self.dwell_time)
-            
-            print("Power adjusted. Moving to next frequency step.")
-            # Reset signal generator to low power
-            self.signal_generator.setPower(self.base_power)
-
-            # Move to the next frequency step
-            current_freq = current_freq + (current_freq * self.sweep_term)
-
-        # Emit the sweep completed signal
-        self.sweepCompleted.emit()
-        '''
         
     def adjust_power_to_target_level(self):
         """Adjust the power level based on probe readings to return to target field level."""
