a893e21ebb30c6994e528eef6551631cef48c532
Bryce Kowalczyk <bkowalczyk@xtrabyteconsulting.com>
2025-02-08 15:01:03 -0500

    Create splash selection dialog for test type

--- AppView.py
+++ AppView.py
@@ -7,25 +7,19 @@ Classes:
     PIDGainsPopUp: A dialog for setting PID controller gains.
     MainWindow: The main application window for the field intensity controller.
 """
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
+
+from PyQt5.QtWidgets import (
+    QApplication, QDialog, QMainWindow, QVBoxLayout,
+    QPushButton, QLabel, QWidget
+)
+
 
 from qt_material import apply_stylesheet
 
-from MainWindow import Ui_MainWindow
-from SignalGenerator import AgilentN5181A, Time, Frequency
-from FieldProbe import ETSLindgrenHI6006
-from FieldController import FieldController
-from LivePlot import FrequencyPlot, PowerPlot
-from PID import PIDController
-from EquipmentLimits import EquipmentLimits
-from ExportWidget import ExportWidget
+from RadiatedImmunity import RadiatedImmunity
 
 import os
 import sys
-import math
-import time
 import numpy as np
 from datetime import datetime
 
@@ -33,6 +27,9 @@ import signal
 from PyQt5.QtCore import QResource
 
 CURRENT_DIR = os.path.curdir
+RADIATED_IMMUNITY = 'Radiated Immunity'
+CONDUCTED_IMMUNITY = 'Conducted Immunity'
+CONDUCTED_IMMUNITY_CALIBRATION = 'Conducted Immunity Calibration'
 
 try:
     # Include in try/except block if you're also targeting Mac/Linux
@@ -42,15 +39,70 @@ try:
 except ImportError:
     pass
 
+class TestSelectionDialog(QDialog):
+    """
+    A simple dialog acting as a splash screen to choose the test type.
+    """
+    def __init__(self, parent=None):
+        super().__init__()
+        self.setWindowTitle('Test Selection')
+        self.selected_test = None
+        
+        layout = QVBoxLayout()
+
+        label = QLabel("Please select the test type:")
+        layout.addWidget(label) 
+
+        self.radiated_button = QPushButton("Radiated Immunity")
+        self.conducted_button = QPushButton("Conducted Immunity")
+        self.conducted_cal_button = QPushButton("Conducted Immunity Calibration")
+        self.radiated_button.clicked.connect(self.openRadiatedImmunity)
+        self.conducted_button.clicked.connect(self.openConductedImmunity)
+        self.conducted_cal_button.clicked.connect(self.openConductedImmunityCalibration)
+        layout.addWidget(self.radiated_button)
+        layout.addWidget(self.conducted_button)
+
+        self.setLayout(layout)
+        
+    def openRadiatedImmunity(self):
+        self.selected_test = RADIATED_IMMUNITY
+        self.accept()
+
+    def openConductedImmunity(self):
+        self.selected_test = CONDUCTED_IMMUNITY
+        self.accept()
+
+    def openConductedImmunityCalibration(self):
+        self.selected_test = CONDUCTED_IMMUNITY_CALIBRATION
+        self.accept()
         
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     #app.setWindowIcon(QtGui.QIcon(':/icons/field_controller.ico'))
-    window = MainWindow()
+
+    # Show the splash/selection dialog.
     
+    selection_dialog = TestSelectionDialog()
     apply_stylesheet(app, theme='dark_cyan.xml')
-    window.show()
+    if selection_dialog.exec_() == QDialog.Accepted:
+        if selection_dialog.selected_test == RADIATED_IMMUNITY:
+            window = RadiatedImmunity()
+        elif selection_dialog.selected_test == CONDUCTED_IMMUNITY:
+            window = ConductedImmunity()
+        elif selection_dialog.selected_test == CONDUCTED_IMMUNITY_CALIBRATION:
+            window = ConductedImmunityCalibration()
+        else:
+            print("No valid test selected. Exiting.")
+            sys.exit(0)
+        apply_stylesheet(app, theme='dark_cyan.xml')
+        window.show()
+    else:
+        # The dialog was cancelled or closed without selection.
+        sys.exit(0)
+    
+    
+    #window.show()
     #app.aboutToQuit(window.killThreads())
     signal.signal(signal.SIGINT, window.closeEvent)
     sys.exit(app.exec_())
