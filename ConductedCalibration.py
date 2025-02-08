dd9c1bf3f072a62564bb22d4a873951b00a7f3bc
Bryce Kowalczyk <bkowalczyk@xtrabyteconsulting.com>
2025-02-08 17:01:50 -0500

    import ui-mainwindow

--- ConductedCalibration.py
+++ ConductedCalibration.py
@@ -1,6 +1,10 @@
 from PyQt5.QtWidgets import *
 
+from ConductedCalibrationWindow import Ui_MainWindow
+
 class ConductedCalibration(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(ConductedCalibration, self).__init__(*args, **kwargs)
         self.setWindowTitle('Conducted Immunity Calibration')
+        self.start_cal_button = QPushButton("Start Calibration")
+        self.start_cal_button.clicked.connect(self.startCalibration)
