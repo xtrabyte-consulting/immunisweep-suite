b95b3b21fd514bffad40041bbf4fc0ac430b6a89
Bryce Kowalczyk <bkowalczyk@xtrabyteconsulting.com>
2025-02-17 18:05:49 -0500

    Update start calibration method

--- ConductedImmunityCalController.py
+++ ConductedImmunityCalController.py
@@ -2,30 +2,29 @@
 # Agilent E4440A Series Spectrum Analyzer
 # Agilent E4421B Series Signal Generator
 # Conducted Immunity 150kHz - 80MHz
+from SpectrumAnalyzer import HPE4440A
+from SignalGenerator import HPE4421B
 
 
 
 class ConductedImmunityCalController:
     
     
-    def __init__(self, spectrum_analyzer, signal_generator):
+    def __init__(self, spectrum_analyzer: HPE4440A, signal_generator: HPE4421B):
         self.spectrum_analyzer = spectrum_analyzer
         self.signal_generator = signal_generator
+        self.signal_generator.frequencySet.connect(self.signal_generator_frequency_set)
+        self.signal_generator.powerSet.connect(self.signal_generator_power_set)
         
-        self.conducted_immunity.start_cal_button.clicked.connect(self.startCalibration)
+
+    def start_calibration(self):
+        self.spectrum_analyzer.set_window(100e3, 100e6)
+        self.spectrum_analyzer.set_units("V")
+        self.spectrum_analyzer.activate_marker()
+        self.spectrum_analyzer.set_frequency(150e3)
+        self.signal_generator.set_frequency(150e3)
+        self.signal_generator.set_power(-20)
         
-    def startCalibration(self):
-        self.spectrum_analyzer.setCenterFrequency(150e3)
-        self.spectrum_analyzer.setSpan(80e6)
-        self.signal_generator.setFrequency(150e3)
-        self.signal_generator.setPower(-20)
-        self.spectrum_analyzer.start()
-        self.signal_generator.start()
-        self.spectrum_analyzer.stop()
-        self.signal_generator.stop()
-        self.spectrum_analyzer.saveData()
-        self.signal_generator.saveData()
-        self.conducted_immunity.close()
-        
-    def run(self):
-        self.conducted_immunity.show()
+    def signal_generator_frequency_set(self):
+        print("Signal Generator Frequency Set")
+
--- ConductedImmunityCalibration.py
+++ ConductedImmunityCalibration.py
@@ -19,7 +19,8 @@ class ConductedImmunityCalibration(QMainWindow, Ui_MainWindow):
         print("Setting spectrum analyzer window to 100 kHz - 100 MHz")
         self.spectrum_analyzer.set_window(100e3, 100e6)
         self.spectrum_analyzer.set_units("V")
-        self.spectrum_analyzer.set_marker_at_frequency(150e3)
+        self.spectrum_analyzer.set_frequency(150e3)
         self.signal_generator.set_frequency(150e3)
         self.signal_generator.set_power(-6)
         #TODO: Set to DC Coupling
+        #TODO: Set to reference level of 5V
